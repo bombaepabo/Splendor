a99bbc1f59ea563472ef7f48e199cecbff53baf6
Nutcharwat <nutcharwat@kkumail.com>
2022-12-23 20:22:30 +0700

    chapter 1 update

---
+++ game_project/Assets/Assets/test/Test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="test" tilewidth="16" tileheight="16" tilecount="45" columns="9">
+ <image source="../../../../../../Sprite/TileMap/chapter 1/test_tile.png" width="144" height="80"/>
+</tileset>
