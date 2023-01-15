401eb75c4f512fe9c2707c4c051aef57eab63184
phakkhapon aksonchanya <55224271+bombaepabo@users.noreply.github.com>
2023-01-15 15:11:47 +0700

    updated map

---
+++ game_project/Assets/Assets/Assets/test/Foozle_2DT0008_Green.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Foozle_2DT0008_Green" tilewidth="16" tileheight="16" tilecount="104" columns="26">
+ <image source="../../../../../../Sprite/TileMap/chapter 1/asset tile/Foozle_2DT0008_GreenValley_Tileset_Pixel_Art.png" width="416" height="64"/>
+</tileset>
